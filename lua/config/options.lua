99df9df231bdc370611be7f44ab62933025df33b
Heiko Noordhof <h.e.noordhof@rug.nl>
2025-10-21 16:26:58 +0200

    4 space indention for C/C++

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -164,7 +164,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 -- Set filetype-specific settings
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup,
-  pattern = { "lua", "python" },
+  pattern = { "lua", "python", "c", "c++" },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
