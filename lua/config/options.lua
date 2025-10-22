0175e8466db3cff9c09467a22447f3be43564317
Heiko Noordhof <h.e.noordhof@rug.nl>
2025-10-22 16:40:25 +0200

    Add commented out line to switch off syntax highlighting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -40,6 +40,7 @@ vim.opt.conceallevel = 0                           -- Don't hide markup
 vim.opt.concealcursor = ""                         -- Don't hide cursor line markup 
 vim.opt.lazyredraw = true                          -- Don't redraw during macros
 vim.opt.synmaxcol = 300                            -- Syntax highlighting limit 
+-- vim.cmd('syntax off')
 
 -- File handling
 vim.opt.backup = false                             -- Don't create backup files
