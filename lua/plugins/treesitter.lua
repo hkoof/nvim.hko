8655fac6e26b8433784b10b7a0f8dc9fafc28067
Heiko Noordhof <h.e.noordhof@rug.nl>
2025-11-11 16:55:54 +0100

    Change treesitter keymaps

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,18 +31,23 @@ return {
                 },
 
                 indent = {
-                    enable = true
+                    enable = true,
+
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
                 },
 
                 incremental_selection = {
                     enable = true,
                     keymaps = {
-                        -- set these to `false` to disable one of the mappings
-                        init_selection = "<Leader>ss",
-                        node_incremental = "<Leader>si",
-                        scope_incremental = "<Leader>sc",
-                        node_decremental = "<Leader>sd",
-                    },
+                        init_selection = "gnn", -- set to `false` to disable one of the mappings
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    }
                 },
 
                 textobjects = {
