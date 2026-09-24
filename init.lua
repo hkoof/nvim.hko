b377078e4dec4632e0832467682c770521a673e0
Heiko Noordhof <h.e.noordhof@rug.nl>
2026-09-24 15:31:37 +0200

    Set default colorscheme to terafox again

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 require("config.options")
 require("config.lazy")
 
-vim.cmd.colorscheme("alabaster")
+vim.cmd.colorscheme("terafox")
+
+-- ngit-tree recommends disabling netrw entirelu
 vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
